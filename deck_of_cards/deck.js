14ba22bb513082c4cfb69cb6a7efabb68915d06d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-05-13 20:21:54 -0700

    added async instance methods to shuffle the deck and draw a card

--- deck_of_cards/deck.js
+++ deck_of_cards/deck.js
@@ -1,12 +1,21 @@
 class Deck {
+    constructor() {
+        this.getDeckInfo()
+    }
     
     async getDeckInfo() {
-        let resp = await axios.get('https://deckofcardsapi.com/api/deck/new/')
-        deckID = resp.data.deck_id;
+        const resp = await axios.get('https://deckofcardsapi.com/api/deck/new/')
+        const deckID = resp.data.deck_id;
         this.deckID = deckID;
     }
     
-    async getDeck() {
-        
+    async shuffleDeck() {
+        await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/shuffle/`)
+    }
+
+    async drawCard() {
+        let resp = await axios.get(`https://deckofcardsapi.com/api/deck/${this.deckID}/draw/?count=1`)
+        let cardIMG = resp.data.cards[0].image;
+        return cardIMG
     }
 }
